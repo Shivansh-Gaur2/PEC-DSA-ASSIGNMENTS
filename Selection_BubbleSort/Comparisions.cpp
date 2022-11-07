bcfbe7b97e74d9faac93a66f4493181bb16a4e22
Shivansh-Gaur2 <shivanshgaur.bt21ele@pec.edu.in>
2022-11-07 12:04:19 +0530

    updated

--- Selection_BubbleSort/Comparisions.cpp
+++ Selection_BubbleSort/Comparisions.cpp
@@ -1,44 +1,50 @@
 #include <iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n){
-    int comparision = 0 , swaps = 0;
-    for(int j = 0; j < n-1; j++){
+void selectionSort(int arr[], int n)
+{
+    int comparision = 0, swaps = 0;
+    for (int j = 0; j < n - 1; j++)
+    {
         int iMin = j;
-        for(int i = j + 1; i < n; i++){
-            cout << "comparing : " << arr[iMin] << " and "<< arr[i]<< endl;
+        for (int i = j + 1; i < n; i++)
+        {
+            cout << "comparing : " << arr[iMin] << " and " << arr[i] << endl;
             comparision += 1;
-            if(arr[i] < arr[iMin]){
+            if (arr[i] < arr[iMin])
+            {
                 iMin = i;
             }
         }
-        if(iMin != j){
+        if (iMin != j)
+        {
             swaps += 1;
-            cout << "swaping:- "<< arr[j] << " and "<< arr[iMin]<< endl;
+            cout << "swaping:- " << arr[j] << " and " << arr[iMin] << endl;
             swap(arr[j], arr[iMin]);
         }
     }
-    cout << " total comparisions = " << comparision << " total swaps:- "<< swaps<< endl;
+    cout << " total comparisions = " << comparision << " total swaps:- " << swaps << endl;
 }
 
-void bubbleSort(int arr[], int n){
+void bubbleSort(int arr[], int n)
+{
     int comparision = 0, swaps = 0;
     for (int i = 1; i < n; i++)
     {
-        for(int j = 0; j < n-1; j++){
-            cout << "comparing "<< arr[j] << " and "<<arr[j+1]<< endl;
+        for (int j = 0; j < n - 1; j++)
+        {
+            cout << "comparing " << arr[j] << " and " << arr[j + 1] << endl;
             comparision += 1;
-            if(arr[j] > arr[j+1]){
-                cout << "Swaping :- "<< arr[j] << " and " << arr[j+1]<< endl;
-                swap(arr[j], arr[j+1]);
+            if (arr[j] > arr[j + 1])
+            {
+                cout << "Swaping :- " << arr[j] << " and " << arr[j + 1] << endl;
+                swap(arr[j], arr[j + 1]);
                 swaps += 1;
             }
         }
     }
-    cout << "comparisions = " << comparision << " swaps:- "<< swaps<< endl;
-   
+    cout << "comparisions = " << comparision << " swaps:- " << swaps << endl;
 }
-
 
 int main()
 {
@@ -46,13 +52,13 @@ int main()
     int arr[5] = {2, 0, 1, 4, 3};
     cout << "SELECTION SORT" << endl;
     selectionSort(arr, n);
-    cout<<"The Sorted Array by using Selection Sort is : ";
-    for(int i = 0; i < n; ++i)
-        cout<<arr[i]<<" ";
+    cout << "The Sorted Array by using Selection Sort is : ";
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
 
-    cout << endl;    
-    cout << "BUBBLE SORT"<< endl;
-    int arr2[5] = { 8, 5, 2, 7, 6 };
+    cout << endl;
+    cout << "BUBBLE SORT" << endl;
+    int arr2[5] = {8, 5, 2, 7, 6};
     bubbleSort(arr2, n);
     cout << "The Sorted Array by using Bubble Sort is : ";
     for (int i = 0; i < n; ++i)
